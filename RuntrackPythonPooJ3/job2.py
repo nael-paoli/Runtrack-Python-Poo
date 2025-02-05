e61972c48b0ea45d1f64d5798ec69b457712b2a5
nael-paoli <nael.paoli@laplateforme.io>
2025-02-05 15:24:17 +0100

    Add files via upload
    job 2 (only the 1st part, for now)

---
+++ RuntrackPythonPooJ3/job2.py
@@ -0,0 +1,8 @@
+class CompteBancaire: 
+  def __init(self, numéro_compte, nom, prénom, solde):
+      self.afficher = numéro_compte, nom, prénom, solde
+      self.afficherSolde = solde
+      self.versement = solde+'montant'
+      self.retrait = solde-'montant'
+
+      ('montant :1500€')
