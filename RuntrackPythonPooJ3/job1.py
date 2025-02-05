63a3c9b6072ae179448d8f5dfb489cd2d4a56b59
nael-paoli <nael.paoli@laplateforme.io>
2025-02-05 15:10:38 +0100

    Add files via upload
    Job 1 - Day 3 commit

---
+++ RuntrackPythonPooJ3/job1.py
@@ -0,0 +1,13 @@
+class Ville: 
+  def __init(self, nom_habitants, nombre_habitants):
+      self.nom_habitants = nom_habitants
+      self.nombre_habitants = nombre_habitants 
+
+class Personne: 
+  def __init(self, nom, age, ajouterPopulation):
+   obj = Ville('Paris','1000000')
+   obj = Ville('Marseille','861635')
+   obj = Personne('John','45 ans','Paris')
+   obj = Personne('Myrtille',"4 ans",'Paris')
+   obj = Personne('Chloé','18 ans','Marseille')
+   
