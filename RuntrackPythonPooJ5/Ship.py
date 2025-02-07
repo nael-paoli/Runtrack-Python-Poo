619c86c3c5e092cbcdad795303c7341a79602932
nael-paoli <nael.paoli@laplateforme.io>
2025-02-07 14:22:56 +0100

    Add files via upload
    Day 5 commit

---
+++ RuntrackPythonPooJ5/Ship.py
@@ -0,0 +1,21 @@
+class Part():
+  def __init__(self,name,material):
+    self.name = "Mât"
+    self.material = "Bois"
+ 
+  object.change_material("new_material")
+
+  str("description_piece")
+
+class Ship (Part):
+  def __parts(self,name,material):
+
+     object.display_state("name","material")
+     object.replace_part("part_name","new_part")
+     object.change_part("part_name","new_material")
+
+class RacingShip (Ship):
+  def __init__(self,max_speed): 
+    self.max_speed = "max_speed"
+
+    object.display_speed()
